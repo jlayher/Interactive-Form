7c71538aea65bb4b2576c267797c5fa978ccb79e
jlayher <john.e.layher@gmail.com>
2020-04-03 14:24:53 -0400

    Added Notes outlining the project

--- interactive-form-v2/js/script.js
+++ interactive-form-v2/js/script.js
@@ -1,1 +1,35 @@
-console.log('Hello');
+/*
+John Layher's JavaScript Project 3
+*/
+
+//Put the first field in the focus state
+
+
+//Add an "Other" option to Job Roles Section
+
+
+//T-Shirt Section
+
+
+//Activity Section
+  //Creating an element to display total activity cost
+  //Listening for changes in the activity Section
+  //Updating andDisplaying the total activity cost
+  //Disabling Conflicting Events
+
+
+//Payment Section
+
+
+
+//Form Validation and Validation Messages
+
+
+//Extra Credit
+  //T-Shirt Section
+    //Hide the color label and select menu until a design has been selected
+  //Conditional Ettor Messages
+    //Program one of your error messages so that more info is provided depending on the error
+  //Real-Time Error messages
+    //Rather than an error message on submit, check for errors as they are being input
+    
